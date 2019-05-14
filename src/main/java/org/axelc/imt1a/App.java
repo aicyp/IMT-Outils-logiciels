bcf590a1be6bee424bc85603fc5947ab73750da5
Axel COUDRAY <axelcoudray1@gmail.com>
2019-05-14 16:20:17 +0200

    initialization

---
+++ src/main/java/org/axelc/imt1a/App.java
@@ -0,0 +1,13 @@
+package org.axelc.imt1a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
